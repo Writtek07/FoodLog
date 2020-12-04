79a132be325135a0ce63dce8cc6c0756b82baeef
Writtek <writtekidccol@gmail.com>
2020-12-05 03:29:10 +0530

    Added my project

--- config/application.rb
+++ config/application.rb
@@ -1,5 +1,6 @@
 require_relative 'boot'
 
+
 require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
@@ -10,6 +11,8 @@ module FoodlogNew
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
+    config.time_zone = 'Mumbai'
+	config.active_record.default_timezone = :local
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
